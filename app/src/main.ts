46ba38bb5390b97b258b29b9f5b36bfc2af96ae5
Diego Chinga Olivera <diego.chinga.olivera@gmail.com>
2017-12-14 17:54:24 -0500

    production build

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,7 +2,7 @@ import './lib'
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
+import { environment } from './environments/environment.prod';
 import { AppModule } from './app/app.module';
 
 if (environment.production) {
